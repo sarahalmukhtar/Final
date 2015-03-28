76489b33635eaf1dd576c98965458a8c6687427e
sarahalmukhtar <sarah.almukhtar@gmail.com>
2015-03-28 15:36:10 -0400

    03_28_15

--- js/script.js
+++ js/script.js
@@ -8,9 +8,30 @@ var map = L.map('map', {
     center: [33.22,43.68],
     zoom: 7,
     minZoom: 6,
-    maxZoom: 20,
+    maxZoom: 14,
     layers: [CartoDBTiles]
 });
+
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend');
+    
+        div.innerHTML += 
+            '<label><input type="checkbox" name="filterCriminal" id="filterCriminal"> Criminal Event</label><br>' + 
+            '<label><input type="checkbox" name="filterExplosive" id="filterExplosive"> Explosive Hazard</label><br>' +
+            '<label><input type="checkbox" name="filterEnemyAction" id="filterEnemyAction"> Enemy Action</label><br>' +
+            '<label><input type="checkbox" name="filterFriendlyAction" id="filterFriendlyAction"> Friendly Action</label><br>' +
+            '<label><input type="checkbox" name="filterFriendlyFire" id="filterFriendlyFire"> Friendly Fire</label><br>' +
+            '<label><input type="checkbox" name="filterNonCombat" id="filterNonCombat"> Non-Combat Event</label><br>' +
+            '<label><input type="checkbox" name="filterSuspicious" id="filterSuspicious"> Suspicious Incident</label><br>' +
+            '<label><input type="checkbox" name="filterThreat" id="filterThreat"> Threat Report</label><br>' +
+            '<label><input type="checkbox" name="filterOther" id="filterOther"> Other</label><br>';
+
+        return div;
+    };
+
+legend.addTo(map);
 
 //var markers = new L.markerClusterGroup({showCoverageOnHover: false});
 var markers = L.featureGroup();
@@ -20,8 +41,7 @@ d3.csv("data/IraqDeathsData.csv", function(data) {
     dataset = data;
     //console.log(dataset);
     latlng(dataset);
-    plotCriminal(dataset);
-    plotExplosive(dataset);
+    plotTotal(dataset);
     //slider(dataset);
 });
 
@@ -65,10 +85,9 @@ function slider(dataset) {
     sliderControl.startSlider();
 }
 
-var criminalEvent = plotCriminal(dataset);
-function plotCriminal(dataset) {
+function plotTotal(dataset) {
     $.each(dataset, function( i, d ) {
-        if(d.Type === 'Criminal Event') {
+        
         var civilian = parseInt(d.Civilian);
         var total = parseInt(d.Total);
         
@@ -117,79 +136,8 @@ function plotCriminal(dataset) {
         marker.setRadius(radius);
         marker.bindPopup(d.Total + " Total<br>" + d.Civilian + " Civilian<br>" + d.Coalition + " Coalition<br>" + d.IraqiForces + " Iraqi Forces<br>" + d.Enemy + " Enemy<br><br>" + d.Type + "<br>" + d.Description, {maxWidth: 200});
         markers.addLayer(marker);
-        }
-        
-    });
-
-    map.addLayer(markers);
-}
-
-var explosiveHazard = plotExplosive(dataset);
-function plotExplosive(dataset) {
-    $.each(dataset, function( i, d ) {
-        if(d.Type === 'Explosive Hazard') {
-        var civilian = parseInt(d.Civilian);
-        var total = parseInt(d.Total);
-        
-        var fillColor = '#000000';
-        if(civilian > 0) {
-            fillColor = '#520000';
-        }
                 
-        var radius = 0;
-        if(total === 1){
-            radius = 2;
-        }
-        if(total > 1 && total <= 5){
-            radius = 3;
-        }
-        if(total > 5 && total <= 15){
-            radius = 4;
-        }
-        if(total > 15 && total <= 30){
-            radius = 5;
-        }
-        if(total > 30 && total <= 60){
-            radius = 6;
-        }
-        if(total > 60 && total <= 100){
-            radius = 8;
-        }
-        if(total > 100 && total <= 300){
-            radius = 12;
-        }
-        if(total > 300){
-            radius = 16;
-        }
-
-        var location = new L.LatLng(d.lat, d.lng);
-        var marker = L.circleMarker(location, {
-            time: d.Time,
-            stroke: true,
-            color: '#ffffff',
-            weight: 0.2,
-            opacity: 1,
-            fillColor: fillColor,
-            fillOpacity: 0.8,
-        });
-
-        marker.setRadius(radius);
-        marker.bindPopup(d.Total + " Total<br>" + d.Civilian + " Civilian<br>" + d.Coalition + " Coalition<br>" + d.IraqiForces + " Iraqi Forces<br>" + d.Enemy + " Enemy<br><br>" + d.Type + "<br>" + d.Description, {maxWidth: 200});
-        markers.addLayer(marker);
-        }
-        
     });
 
     map.addLayer(markers);
 }
-
-var baseMaps = {
-    "CartoDB Light": CartoDBTiles
-};
-
-var overlayMaps = {
-    "Criminal Event<br>(Arson, Carjacking, Kidnapping, Looting, Murder, Sabotage, Shooting, Smuggling, Theft": criminalEvent,
-    "Explosive Hazard": explosiveHazard
-};
-
-L.control.layers(baseMaps, overlayMaps).addTo(map);
